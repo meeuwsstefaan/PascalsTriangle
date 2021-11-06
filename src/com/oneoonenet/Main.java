90a457a69d1eeffe5b53f57a02e039252b83b172
meeuwsstefaan <stefaanmeeuws@gmail.com>
2021-11-06 07:47:16 +0100

    Changed space placeholders.

--- src/com/oneoonenet/Main.java
+++ src/com/oneoonenet/Main.java
@@ -7,14 +7,14 @@ class GFG {
         for (int line = 1; line <= n; line++) {
             for (int j = 0; j <= n - line; j++) {
                 // for left spacing
-                System.out.print("-");
+                System.out.print("+");
             }
 
             // used to represent C(line, i)
             int C = 1;
             for (int i = 1; i <= line; i++) {
                 // The first value in a line is always 1
-                System.out.print(C + "+");
+                System.out.print(C + "_");
                 C = C * (line - i) / i;
             }
             System.out.println();
