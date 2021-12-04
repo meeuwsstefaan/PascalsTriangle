314a0f2d52b7ed907c232c6b7079c027372dd4d5
meeuwsstefaan <stefaanmeeuws@gmail.com>
2021-12-04 01:43:17 +0100

    Formatted code.

--- src/com/oneoonenet/Main.java
+++ src/com/oneoonenet/Main.java
@@ -1,29 +1,39 @@
-package com.oneoonenet;// Print Pascal's Triangle in Java
+package com.oneoonenet;
 
+/**
+ * Print Pascal's Triangle in Java
+ */
 class GFG {
 
-    // Pascal function
+    /**
+     * Driver code
+     * @param args = Program arguments
+     */
+    public static void main(String[] args) {
+        int n = 8;
+        printPascal(n);
+    }
+
+    /**
+     * Pascal function
+     * @param n = the height of Pascal's triangle
+     */
     public static void printPascal(int n) {
+
         for (int line = 1; line <= n; line++) {
             for (int j = 0; j <= n - line; j++) {
                 // for left spacing
                 System.out.print("+");
             }
 
-            // used to represent C(line, i)
-            int C = 1;
+            // used to represent c(line, i)
+            int c = 1;
             for (int i = 1; i <= line; i++) {
                 // The first value in a line is always 1
-                System.out.print(C + "_");
-                C = C * (line - i) / i;
+                System.out.print(c + "_");
+                c = c * (line - i) / i;
             }
             System.out.println();
         }
-    }
-
-    // Driver code
-    public static void main(String[] args) {
-        int n = 8;
-        printPascal(n);
     }
 }
